209d7087e5a9a0b62775e3c75ee7c5bd17ffa93b
Maksim Stankevic <maksim.stankevic1@gmail.com>
2019-08-13 21:07:02 +0300

    Initial commit

---
+++ src/main/java/local/ndaqfacts/NdaqfactsApplication.java
@@ -0,0 +1,13 @@
+package local.ndaqfacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdaqfactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NdaqfactsApplication.class, args);
+	}
+
+}
